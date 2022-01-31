210752a77f9189a62c003706092cb9d66bdb9694
ace-krypton <ramizna@code.edu.az>
2022-01-31 22:19:39 +0100

    Adding Character Create

---
+++ src/CreateCharacter.java
@@ -0,0 +1,18 @@
+public class CreateCharacter {
+
+    //Methods
+    public void menuForCharacterCreating() {
+        System.out.println("+---------------------------------------------------------------+");
+        System.out.println("[Page ONE]                                       [Tutorial Off]  ");
+        System.out.println("+---------------------------------------------------------------+");
+        System.out.println("[1] New Barbarian");
+        System.out.println("[2] New Paladin (Under Development)");
+        System.out.println("[3] New Assassin (Under Development)");
+        System.out.println("[4] New Wizard (Under Development)");
+        System.out.println("[5] New Druid (Under Development)");
+        System.out.println("[6] New Ranger (Under Development)");
+        System.out.println("[Q] Swap                                               [A] Back  ");
+        System.out.println("+---------------------------------------------------------------+");
+        System.out.print("> ");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //System Objects
+        Scanner scan = new Scanner(System.in);
 
+        //Methods
+        menu();
+
+        //User Input
+        System.out.print("-> ");
+        int input = scan.nextInt();
     }
 
-    public void menu() {    //Menu for start screen
-
+    public static void menu() {    //Menu for start screen
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("[1] Play");
+        System.out.println("[2] Settings");
+        System.out.println("[3] Trials");
+        System.out.println("[4] Credits\n");
+        System.out.println("[X] Exit");
+        System.out.println("---------------------------------------------------------------");
     }
 }
